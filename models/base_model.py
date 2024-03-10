a318d223a160f3c037341d957cafcccf8fe6b821
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-10 05:28:41 +0100

    created BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,26 @@ class BaseModel:
     Base class for other classes with common attributes/methods.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes BaseModel instance with unique id and current datetime.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
